6d32aa2f280f0f4ae3a6bf59d2ab46bd391317c2
Zachary Jones <zacharytamas@gmail.com>
2016-06-19 17:58:56 -0400

    Lab 2

---
+++ src/Lab02/Practice_2_4.java
@@ -0,0 +1,24 @@
+package Lab02;
+// Class: CS 1301/01
+// Term: Summer 2016
+// Name: Zachary Jones
+// Instructor: Chad Teat
+// Lab: 2
+
+import java.util.Scanner;
+
+public class Practice_2_4 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the length of a side of a square: ");
+        int side = input.nextInt();
+
+        System.out.format("Area: %d\n", side * side);
+        System.out.format("Perimeter: %d\n", side * 4);
+
+    }
+
+}

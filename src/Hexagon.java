741293c9e7f54f3abf1be4dfa0fcd633fb54173a
Zachary Jones <zacharytamas@gmail.com>
2016-07-02 18:49:24 -0400

    Finished second project of Lab 5

---
+++ src/Hexagon.java
@@ -0,0 +1,24 @@
+
+// Class: CS 1301/01
+// Term: Summer 2016
+// Name: Zachary Jones
+// Instructor: Chad Teat
+// Lab: 5
+
+import java.util.Scanner;
+
+public class Hexagon {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the side: ");
+        double s = scanner.nextDouble();
+
+        double area = (6 * Math.pow(s, 2)) / (4 * Math.tan(22./7/6));
+
+        System.out.format("The area of the hexagon is %.2f\n", area);
+
+    }
+
+}

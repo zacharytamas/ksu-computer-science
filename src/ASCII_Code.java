0af9ee01b6f25267fa31c75e3d289ebecd0094c3
Zachary Jones <zacharytamas@gmail.com>
2016-07-02 18:57:44 -0400

    Finished exercise 3 of Lab 5

---
+++ src/ASCII_Code.java
@@ -0,0 +1,23 @@
+
+// Class: CS 1301/01
+// Term: Summer 2016
+// Name: Zachary Jones
+// Instructor: Chad Teat
+// Lab: 5
+
+import java.util.Scanner;
+
+public class ASCII_Code {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an ASCII code: ");
+        int code = scanner.nextInt();
+
+        System.out.format("The character for ASCII code %d is %c\n", code, (char) code);
+
+    }
+
+}

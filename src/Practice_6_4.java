7ec5c0e19a1f451c15e7c6825326c0c3e5b6c007
Zachary Jones <zacharytamas@gmail.com>
2016-07-10 16:53:52 -0400

    Finish Lab 6.4

---
+++ src/Practice_6_4.java
@@ -0,0 +1,27 @@
+
+// Class: CS 1301/01
+// Term: Summer 2016
+// Name: Zachary Jones
+// Instructor: Chad Teat
+// Lab: 6
+
+import java.util.Scanner;
+
+public class Practice_6_4 {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter a string: ");
+    String entry = scanner.nextLine();
+
+    System.out.println("Entered String: " + entry);
+
+    for (int i = 1; i <= entry.length(); i++) {
+      System.out.format("Character #%d:\t %s\n", i, entry.charAt(i - 1));
+    }
+
+  }
+
+}

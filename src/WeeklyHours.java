f23c49e26aa09a790923a53298317813afb4623c
Zachary Jones <zacharytamas@gmail.com>
2016-07-23 18:33:30 -0400

    Started on this but I think it may actually be optional. Asked for clarification in the forums. Pausing on this for now.

---
+++ src/WeeklyHours.java
@@ -0,0 +1,26 @@
+
+// Class: CS 1301/01
+// Term: Summer 2016
+// Name: Zachary Jones
+// Instructor: Chad Teat
+// Assignment 11
+
+import java.util.Scanner;
+
+public class WeeklyHours {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            
+
+            System.out.print("\nAgain? y/n ");
+            if (scanner.next().toLowerCase().charAt(0) == 'n') break;
+        }
+
+    }
+
+}

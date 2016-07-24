8b725462dfdbd13b0e71c74a060ecdc860b04877
Zachary Jones <zacharytamas@gmail.com>
2016-07-24 14:04:21 -0400

    Finish Assignment 12.1

---
+++ src/TestRectangle.java
@@ -0,0 +1,31 @@
+
+// Class: CS 1301/01
+// Term: Summer 2016
+// Name: Zachary Jones
+// Instructor: Chad Teat
+// Assignment 12
+
+import java.util.Scanner;
+
+public class TestRectangle {
+
+    public static void main(String[] args) {
+
+        Rectangle rect1 = new Rectangle();
+        Rectangle rect2 = new Rectangle(5, 5);
+
+        Rectangle[] rects = {rect1, rect2};
+
+        for (int i = 0; i < rects.length; i++) {
+            Rectangle rect = rects[i];
+            System.out.println("Rectangle #" + (i + 1));
+            System.out.printf("Width: %.1f\n", rect.width);
+            System.out.printf("Height: %.1f\n", rect.height);
+            System.out.printf("Area: %.3f\n", rect.getArea());
+            System.out.printf("Perimeter: %.1f\n", rect.getPerimeter());
+            System.out.println();
+        }
+
+    }
+
+}

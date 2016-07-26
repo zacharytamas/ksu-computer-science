8137bd49061e5668a9f0cc266d63d5bd991bf993
Zachary Jones <zacharytamas@gmail.com>
2016-07-26 17:20:53 -0400

    Finish Lab 13

---
+++ src/Stock.java
@@ -0,0 +1,24 @@
+
+// Class: CS 1301/01
+// Term: Summer 2016
+// Name: Zachary Jones
+// Instructor: Chad Teat
+// Lab 13
+
+public class Stock {
+
+    String symbol;
+    String name;
+    double previousClosingPrice;
+    double currentPrice;
+
+    public Stock(String symbol, String name) {
+        this.symbol = symbol;
+        this.name = name;
+    }
+
+    public double getChangePercent() {
+        return (currentPrice / previousClosingPrice - 1.0) * 100;
+    }
+
+}
---
+++ src/TestStock.java
@@ -0,0 +1,31 @@
+
+// Class: CS 1301/01
+// Term: Summer 2016
+// Name: Zachary Jones
+// Instructor: Chad Teat
+// Lab 13
+
+import java.util.Scanner;
+
+public class TestStock {
+
+    public static void main(String[] args) {
+
+        Stock ORCL = new Stock("ORCL", "Oracle");
+        ORCL.previousClosingPrice = 34.5;
+        ORCL.currentPrice = 34.35;
+        System.out.printf("ORCL change is %.2f%%\n", ORCL.getChangePercent());
+
+        Stock MSFT = new Stock("MSFT", "Microsoft");
+        MSFT.previousClosingPrice = 56.57 - .77;
+        MSFT.currentPrice = 56.57;
+        System.out.printf("MSFT change is %.2f%%\n", MSFT.getChangePercent());
+
+        Stock GOOG = new Stock("GOOG", "Alphabet");
+        GOOG.previousClosingPrice = 738.63;
+        GOOG.currentPrice = 742.74;
+        System.out.printf("GOOG change is %.2f%%\n", GOOG.getChangePercent());
+
+    }
+
+}

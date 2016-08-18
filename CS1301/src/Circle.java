cb47812f4626c57ca85197619de0ac35228e4ffd
Zachary Jones <zacharytamas@gmail.com>
2016-08-18 16:12:42 -0400

    Restructure again!

---
+++ CS1301/src/Circle.java
@@ -0,0 +1,22 @@
+/**
+ * Created by zacharytamas on 8/18/16.
+ */
+public class Circle {
+
+    private double radius = 1.0;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}

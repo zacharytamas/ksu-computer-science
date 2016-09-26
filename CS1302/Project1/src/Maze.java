af122865715feb06c707318d57c8964025296017
Zachary Jones <zacharytamas@gmail.com>
2016-09-26 17:28:49 -0400

    Start Project1

---
+++ CS1302/Project1/src/Maze.java
@@ -0,0 +1,26 @@
+/**
+ * Created by zacharytamas on 9/26/16.
+ */
+public class Maze {
+    private int[][] maze;
+
+    public Maze(int[][] mazeArray) {
+        this.maze = mazeArray;
+    }
+
+    public void displayMaze() {
+
+    }
+
+    public void takeStep() {
+
+    }
+
+    public void findExit() {
+
+    }
+
+    public void displayPath() {
+        
+    }
+}

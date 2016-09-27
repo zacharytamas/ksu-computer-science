dbc85a4b35172a280c409cb0b5973121bc029692
Zachary Jones <zacharytamas@gmail.com>
2016-09-27 18:10:08 -0400

    Preparing to have a mouse running around in here.

--- CS1302/Project1/src/Maze.java
+++ CS1302/Project1/src/Maze.java
@@ -2,6 +2,10 @@
  * Created by zacharytamas on 9/26/16.
  */
 public class Maze {
+    public static final int WALL = 0;
+    public static final int OPEN = 1;
+    public static final int MOUSE = 5;
+
     private int[][] maze;
 
     public Maze(int[][] mazeArray) {
@@ -11,6 +15,19 @@ public class Maze {
     public void displayMaze() {
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze[i].length; j++) {
+                String cellOutput;
+
+                switch (maze[i][j]) {
+                    case Maze.WALL:
+                        cellOutput = "# ";
+                        break;
+                    case Maze.OPEN:
+                        cellOutput = "  ";
+                        break;
+                    case Maze.MOUSE:
+                        cellOutput = "@ ";
+                        break;
+                }
                 if (maze[i][j] == 0) {
                     System.out.print("# ");
                 } else if (maze[i][j] == 1) {
